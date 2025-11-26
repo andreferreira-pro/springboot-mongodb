8ad3b14a5c68f9f907416b9782ce75f353c0b38e
Andre Ferreira <andre_ferreiraa@hotmail.com>
2025-11-26 17:47:15 -0300

    f:refatorado classes UserResource e UserService e implementado tratamento de erro 404 para requisicoes get by id

--- src/main/java/com/andreferreira/springbootmongo/resources/UserResource.java
+++ src/main/java/com/andreferreira/springbootmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ import com.andreferreira.springbootmongo.dto.UserDTO;
 import com.andreferreira.springbootmongo.service.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value ="/users")
 public class UserResource {
 
 	@Autowired
@@ -28,5 +29,19 @@ public class UserResource {
 		List<UserDTO> listDto = listUsers.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
 											
 		return ResponseEntity.ok().body(listDto);
-	}	
+	}
+	
+	@GetMapping
+	@RequestMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		
+		User userById = userService.findById(id);
+		
+		return ResponseEntity.ok().body(new UserDTO(userById));
+	}
+	
+	
+	
+	
+	
 }
