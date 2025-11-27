079f640ed0cfbdd9027e438b199449acdb3e8aba
Andre Ferreira <andre_ferreiraa@hotmail.com>
2025-11-27 10:59:56 -0300

    f:referenciacao Post em User

--- src/main/java/com/andreferreira/springbootmongo/config/Instantiation.java
+++ src/main/java/com/andreferreira/springbootmongo/config/Instantiation.java
@@ -42,6 +42,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null,sdf.parse("23/03/2025"), "Bom dia!!!", "Acordei Feliz hoje!", new AuthorDTO(maria));
 		
 		postResitory.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 	}
 
 }
