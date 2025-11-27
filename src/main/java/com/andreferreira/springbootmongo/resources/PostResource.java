e01ab67ef2ff0c6902ecb2f7a8370105196a475e
Andre Ferreira <andre_ferreiraa@hotmail.com>
2025-11-27 15:08:56 -0300

    f:inclusao busca com varios parametros

--- src/main/java/com/andreferreira/springbootmongo/resources/PostResource.java
+++ src/main/java/com/andreferreira/springbootmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.andreferreira.springbootmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,23 @@ public class PostResource {
 		return ResponseEntity.ok().body(postList);
 		
 		
+	}
+	
+	@GetMapping("/fullsearch")
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		
+		text = URL.decodePara(text);
+		
+		Date min = URL.convertDate(minDate, new Date(0L));
+		
+		Date max = URL.convertDate(maxDate, new Date());
+		
+		List<Post> list = postService.fullSearch(text, min, max);
+		
+		return ResponseEntity.ok().body(list);
 	}
 	
 		
